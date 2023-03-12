b7861209697e074bf7b69e190f29f9e7a8bb16f1
Zwirled <bramwellamelia@gmail.com>
2023-03-12 18:17:36 +0000

    Added Project Gallery to Projects page, added project component to project gallery

--- src/components/ProjectGallery/index.js
+++ src/components/ProjectGallery/index.js
@@ -1,7 +1,11 @@
+import React from 'react';
+import Project from '../Project';
+
 function ProjectGallery() {
     return (
         <div>
             <h1>This is the project gallery</h1>
+            <Project />
         </div>
     );
 }
