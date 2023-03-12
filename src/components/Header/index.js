c4dd76f6b70a18febc66775e00f9cdc3181d36d4
Zwirled <bramwellamelia@gmail.com>
2023-03-12 12:54:15 +0000

    Got basic page routes working, basic global header added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,30 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 function Header() {
+    const pages = [
+        { name: 'Home', path: '/', end: true },
+        { name: 'About', path: '/about' },
+        { name: 'Projects', path: '/projects' },
+        { name: 'Contact', path: '/contact' },
+    ];
+
     return (
-        <div>
-            <h1>This is the header</h1>
-        </div>
+        <ul className="nav nav-tabs">
+            {pages.map((page) => (
+                <li className="nav-item" key={page.path}>
+                    <NavLink
+                        to={page.path}
+                        end={page.end}
+                        className={({ isActive }) =>
+                            isActive ? 'nav-link active' : 'nav-link'
+                        }
+                    >
+                        {page.name}
+                    </NavLink>
+                </li>
+            ))}
+        </ul>
     );
 }
 
