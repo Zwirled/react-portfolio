bdd34103eac888f2a1ddc9d7124aac55f842fe94
Zwirled <bramwellamelia@gmail.com>
2023-03-12 11:12:27 +0000

    Projects added to project.json file

---
+++ src/components/Project/index.js
@@ -0,0 +1,9 @@
+function Project() {
+    return (
+        <div>
+            <h1>This is the project page</h1>
+        </div>
+    );
+}
+
+export default Project;
---
+++ src/components/ProjectGallery/index.js
@@ -0,0 +1,9 @@
+function ProjectGallery() {
+    return (
+        <div>
+            <h1>This is the project gallery</h1>
+        </div>
+    );
+}
+
+export default ProjectGallery;
